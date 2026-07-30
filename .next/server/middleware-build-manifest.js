b785ae1bb7448e1168c63aaf92b929a07fd5b5fb
phaitso <phanitso494@gmail.com>
2026-07-30 11:58:41 +0700

    Updated trace

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -34,6 +34,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/chat.js"
     ],
+    "/contact": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/contact.js"
+    ],
     "/marketplace": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "409afcd207d3180b"; }
+/******/ 		__webpack_require__.h = function() { return "2432076859507567"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
